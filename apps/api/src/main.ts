a04ef7337f3842db31b3ca82b9b60946d4165c0a
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2022-10-29 09:18:55 +0600

    domain

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
       cookie: {
         maxAge: 30 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true,
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        sameSite: "none",
         domain:'.railway.app',
         secure: true
       },
